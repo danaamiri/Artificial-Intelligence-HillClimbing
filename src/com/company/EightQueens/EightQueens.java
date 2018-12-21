4f089f363c8b510cccc4cb90108a792bb5675213
mahsa-mokhtare <mokhtare.mahsa@yahoo.com>
2018-12-22 01:44:56 +0330

    Game Generators Done (log canceled)

--- src/com/company/EightQueens/EightQueens.java
+++ src/com/company/EightQueens/EightQueens.java
@@ -7,8 +7,5 @@ public class EightQueens {
     private GameGenerator gameGenerator = new GameGenerator();
     private StateGenerator stateGenerator = new StateGenerator();
     public EightQueens() {
-        for(int i=0;i<12;i++){
-            System.out.println(gameGenerator.printGame(stateGenerator.generateStates().get(i)));
-        }
     }
 }
